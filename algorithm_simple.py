b13539ef22997b7266b9a6f0ef195a70c78c0f93
Gillian <gilsch02@gmail.com>
2023-06-28 16:48:46 +0200

    Cleaned file

--- algorithm_simple.py
+++ algorithm_simple.py
@@ -1,17 +1,21 @@
 import numpy as np
 import pandas as pd
 
-def run_simple_algorithm(img: np.ndarray) -> np.ndarray:
-    """
 
-    :param img: np.ndarray with all the pixel values.
+def run_simple_algorithm(img: np.ndarray):
     """
+    Applies the simple algorithm to compress an input image.
 
+    Args:
+        img (numpy.ndarray): The input image.
+
+    Returns:
+        np.ndarray: The compressed image values.
+    """
     img = colour_compress(img)
     img = run_length_enc(img)
 
     return img
-
 
 
 def colour_compress(img: np.ndarray):
@@ -31,6 +35,7 @@ def colour_compress(img: np.ndarray):
     for y in range(img.shape[0]):
         for x in range(img.shape[1]):
             rgb = []
+            # Append each floored value of RGB to the RGB list
             for i in range(len(img[y, x])):
                 rgb.append(img[y, x][i] - (img[y, x][i] % 4))
             img[y, x] = np.array(rgb)
@@ -54,17 +59,20 @@ def run_length_enc(img: np.ndarray) -> list:
     for y in range(img.shape[0]):
         row = []
         count = 1
-        for x in range(img.shape[1]-1):
-            if img[y, x][0] == img[y, x + 1][0] and img[y, x][1] == img[y, x + 1][1] and img[y, x][2] == img[y, x + 1][2]:
+        for x in range(img.shape[1] - 1):
+            # If the RGB values are identical, add to the counter
+            if img[y, x][0] == img[y, x + 1][0] and \
+                    img[y, x][1] == img[y, x + 1][1] and \
+                    img[y, x][2] == img[y, x + 1][2]:
                 count += 1
             else:
+                # Append the last count to the row and reset the counter
                 row.append((img[y, x].tolist(), count))
                 count = 1
         row.append((img[y, x].tolist(), count))
         img_rle.append(row)
 
     return img_rle
-
 
 
 def build_image(image_name: str, directory="images/STORE/"):
@@ -79,9 +87,7 @@ def build_image(image_name: str, directory="images/STORE/"):
         np.ndarray: The reconstructed image as a NumPy array.
 
     """
-    print("1:", image_name)
-    # Open the data
-    print("2: " + image_name)
+    # Open the compressed data
     rle = pd.read_pickle(f'{directory}{image_name}')
 
     # Initialize the array with the original resolution
@@ -89,13 +95,8 @@ def build_image(image_name: str, directory="images/STORE/"):
     for y in range(len(rle)):
         row = []
         for x in range(len(rle[y])):
+            # Append the value to the list the counter amount
             for i in range(rle[y][x][1]):
                 row.append(rle[y][x][0])
         res.append(row)
     return np.array(res, dtype=np.uint8)
-
-
-
-if __name__ == "__main__":
-    # test_algorithm()
-    pass
