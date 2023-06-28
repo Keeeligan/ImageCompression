2ab2091b72f0d04de4f0b41a2576d56d0633c774
Gillian <gilsch02@gmail.com>
2023-06-28 16:52:49 +0200

    Cleaned file

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import pygame as pg
 import image_main as i_main
-
 import os
+
 
 def run_app():
     pg.init()
@@ -22,12 +22,12 @@ def run_app():
     font = pg.font.Font(None, 28)
 
     # Make the buttons for compression
-    s_compress_button = pg.Rect(width/2-250/2, 50, 250, 50)
+    s_compress_button = pg.Rect(width / 2 - 250 / 2, 50, 250, 50)
     text = "Simple compression"  # Text to display within the button
     text_s_comp = font.render(text, True, (0, 0, 0))
     text_s_comp_rect = text_s_comp.get_rect(center=s_compress_button.center)  # Center the text within the button
 
-    s_build_button = pg.Rect(width/2-250/2, 150, 250, 50)
+    s_build_button = pg.Rect(width / 2 - 250 / 2, 150, 250, 50)
     text = "Simple build"  # Text to display within the button
     text_s_build = font.render(text, True, (0, 0, 0))
     text_s_build_rect = text_s_build.get_rect(center=s_build_button.center)  # Center the text within the button
@@ -47,14 +47,13 @@ def run_app():
     selected_in_img = ""
     update_in_image = False
 
-    img_out = pg.Rect(width-450, 50, 400, 400)
+    img_out = pg.Rect(width - 450, 50, 400, 400)
     selected_out_img = ""
     update_out_image = False
 
     # Draw the images on the canvas
     pg.draw.rect(canvas, (150, 150, 150), img_in)
     pg.draw.rect(canvas, (150, 150, 150), img_out)
-
 
     # Start running
     while not exit:
@@ -77,7 +76,9 @@ def run_app():
 
                 if s_build_button.collidepoint(event.pos):
                     # Only build the data if it was produced with the right algorithm
-                    if selected_out_img != "" and selected_out_img[len(selected_out_img)-len(".pickle")-len("simp"):len(selected_out_img)-len(".pickle")] == "simp":
+                    if selected_out_img != "" and selected_out_img[
+                                                  len(selected_out_img) - len(".pickle") - len("simp"):len(
+                                                          selected_out_img) - len(".pickle")] == "simp":
                         print("Starting simple building")
                         print(selected_out_img)
                         i_main.build_alg_s(selected_out_img)
@@ -85,19 +86,19 @@ def run_app():
 
                 if c_compress_button.collidepoint(event.pos):
                     if selected_in_img != "":
-                        ("Starting complex compression")
+                        print("Starting complex compression")
                         print(selected_in_img)
                         i_main.compress_alg_c(selected_in_img)
 
                 if c_build_button.collidepoint(event.pos):
                     # Only build the data if it was produced with the right algorithm
-                    if selected_out_img != "" and selected_out_img[len(selected_out_img) - len(".pickle") - len("simp"):len(selected_out_img) - len(".pickle")] == "comp":
+                    if selected_out_img != "" and selected_out_img[
+                                                  len(selected_out_img) - len(".pickle") - len("simp"):len(
+                                                          selected_out_img) - len(".pickle")] == "comp":
                         print("Starting complex building")
                         print(selected_out_img)
                         i_main.build_alg_c(selected_out_img)
                         update_out_image = True
-
-
 
                 # Check if a click occurred on an input file name
                 for i, file in enumerate(input_files):
@@ -118,9 +119,6 @@ def run_app():
                         print("Clicked on output file:", file)
                         selected_out_img = file
                         # Perform further actions for the selected file
-
-
-
 
         # Draw the buttons on the canvas
         pg.draw.rect(canvas, (200, 200, 200), s_compress_button)
@@ -143,7 +141,7 @@ def run_app():
         text_y = 500  # Initial y-coordinate for text
         for file in data_files:
             text = font.render(file, True, (0, 0, 0), (200, 200, 200))
-            canvas.blit(text, (width-450, text_y))
+            canvas.blit(text, (width - 450, text_y))
             text_y += 30
 
         if update_in_image:
@@ -154,7 +152,7 @@ def run_app():
 
         if update_out_image:
             print("Updating out image")
-            image_name = selected_out_img[:len(selected_out_img)-len(".pickle")]
+            image_name = selected_out_img[:len(selected_out_img) - len(".pickle")]
             out_image = pg.image.load("images/OUT/" + image_name + ".png")
             resized_image = pg.transform.scale(out_image, (img_out.width, img_out.height))
             canvas.blit(resized_image, (img_out.x, img_out.y))
